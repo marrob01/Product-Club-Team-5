4e5a0993ebfdc3d5d405087017b65cd50d080f43
stacy leitstein <unique523@yahoo.com>
2021-10-14 16:39:00 -0400

    having issue with props

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Navigation from './Global/Nav.js'
 import Test from './Test.js'
 import { BrowserRouter,  Router, Switch, Route } from "react-router-dom";
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useHistory } from 'react';
 import LandingPage from './Home/LandingPage';
 import Contact from './Contactpage/Contact.js';
 import About from './About/About.js';
@@ -14,35 +14,56 @@ import './About/About.css';
 import StateData from './StatePage/StateData.js';
 import FederalData from './FederalPage/FederalData.js';
 
-function App() {
+function App(props) {
   const [user, setUser] = useState(null)
-  const [data,setData]=useState([])
+  const [federalData,setFederalData]=useState([])
+  const [stateData,setStateData]=useState([])
   const [filteredDataList, setFilteredDataList]=useState([])
   const [searchInput, setSearchInput]=useState([])
   // const history = useHistory()
 
-  // const getData=()=>{
-  //   fetch('http://localhost:9000/skills'
-  //
-  //   ,{
-  //     headers : {
-  //       'Content-Type': 'application/json',
-  //       'Accept': 'application/json'
-  //      }
-  //   }
-  //   )
-  //     .then(function(response){
-  //       // console.log(response)
-  //       return response.json();
-  //     })
-  //     .then(function(myJson) {
-  //       console.log(myJson);
-  //       setData(myJson)
-  //     });
-  // }
-  // useEffect(()=>{
-  //   getData()
-  // },[])
+  const getFederalData=()=>{
+    fetch('http://localhost:9000/federal'
+  
+    ,{
+      headers : {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+       }
+    }
+    )
+      .then(function(response){
+        // console.log(response)
+        return response.json();
+      })
+      .then(function(federalData) {
+        // console.log(federalData);
+        setFederalData(federalData)
+      });
+  }
+  const getStateData = ()=>{
+   fetch('http://localhost:9000/state'
+  
+    ,{
+      headers : {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+       }
+    }
+    )
+      .then(function(response){
+        console.log(response)
+        return response.json();
+      })
+      .then(function(myJson) {
+        console.log(myJson);
+        setStateData(stateData=myJson)
+      });
+  }
+  useEffect(()=>{
+    getStateData()
+    getFederalData()
+  },[])
 
 
 
@@ -53,16 +74,15 @@ function App() {
 
           <Switch>
             <Route exact path='/' component={LandingPage}/>
-
             <Route exact path="/about" component={About} />
-            <Route exact path="/state" component={StateData} />
+          <Route exact path="/state" component={StateData} state={stateData} />
             <Route exact path="/federal" component={FederalData} />
             <Route exact path="/contact" component={Contact} />
           </Switch>
 
           <Footer />
         </BrowserRouter >
-
+        
     </div>
   );
 }
--- src/StatePage/StateData.js
+++ src/StatePage/StateData.js
@@ -1,8 +1,19 @@
 import React from 'react'
 
-function StateData() {
+function StateData(props) {
+    console.log(props.state);
     return (
         <div>
+            <div className="state-header">
+            <header>ADVANCE STATE DATA SEARCH</header>
+            </div>
+            {/* <div className="state-data">
+                stateData.map(state, index) => {
+                    <div> {stateData.recipientName} </div>
+
+                } 
+            </div> */}
+
             
         </div>
     )
