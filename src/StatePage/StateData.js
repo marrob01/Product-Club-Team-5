7a3de2ff5bc5162aae656606f14d6ef707993478
stacy leitstein <unique523@yahoo.com>
2021-10-16 15:49:27 -0400

    completed getting all the data

--- src/StatePage/StateData.js
+++ src/StatePage/StateData.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
+import BootstrapTable from 'react-bootstrap-table-next'
 
 
 function StateData() {
 
     const [stateData, setStateData] = useState([])
 
-    const getStateData = () => {
-        fetch('http://localhost:9000/state'
+    const getStateData = async () => {
+        await fetch('http://localhost:9000/state'
 
             , {
                 headers: {
@@ -25,32 +26,81 @@ function StateData() {
                 setStateData(myJson)
             });
     }
+
     useEffect(() => {
         getStateData()
     }, [])
 
-    const stateMap = 
-        stateData.map((state, i) => {
-          return (
-                <div key={i}>
-                    <ul>
-                        <li>{state.grant.recipientName}</li>
-                        <li className="awardId">{state.grant.awardID} </li>
-                        {/* <li className */}
-                    </ul>
+    
+    
+    const stateMap =
+    stateData.map((state, i) => {
+        return (
+            <div key={i}>
+                    <table>
+                        {/* <caption>ADVANCE STATE DATA SEARCH</caption> */}
+                        <thead> spending prime award</thead>
+                        <tbody>
+                            <tr>
+                                <th scope="col">{state.grant.recipientName}</th>
+                                <td className="awardId">{state.grant.awardID} </td>
+                                <td className="startDate">{state.grant.startDate}</td>
+                                <td className="endDate"> {state.grant.endDate}</td>
+                                <td className="county">{state.grant.County}</td>
+                                <td className="city">{state.grant.City}</td>
+                                <td className="covidObligations">{state.grant.covidObligations}</td>
+                                <td className="awardAmount">{state.grant.awardAmount}</td>
+                                <td className="awardType"> {state.grant.awardType}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <table>
+                        <tr>
+                            <th scope="col">{state.directPayment.recipientName}</th>
+                            <td className="awardId">{state.directPayment.awardID} </td>
+                            <td className="startDate">{state.directPayment.startDate}</td>
+                            <td className="endDate"> {state.directPayment.endDate}</td>
+                            <td className="state">{state.directPayment.State}</td>
+                            <td className="county">{state.directPayment.County}</td>
+                            <td className="city">{state.directPayment.City}</td>
+                            <td className="covidObligations">{state.directPayment.covidObligations}</td>
+                            <td className="awardAmount">{state.directPayment.awardAmount}</td>
+                            <td className="awardType"> {state.directPayment.awardType}</td>
+                        </tr>
+                    </table>
                 </div>
-         )}
-         )
-     
-           
+            )
+        }
+        )
+        
+        const columns = [{
+            dataField: 'id',
+            text: 'Product ID'
+        }, {
+            dataField: 'name',
+            text: 'Product Name'
+        }, {
+            dataField: 'price',
+            text: 'Product Price'
+        }];
+    
+        < BootstrapTable
+            keyField="id"
+            data={stateMap}
+            columns={columns}
+            striped
+            hover
+            condensed
+        />
+
     return (
         <div>
             <div className="state-header">
-            <header>ADVANCE STATE DATA SEARCH</header>
+                <header>ADVANCE STATE DATA SEARCH</header>
             </div>
             <div className="state-data">
                 {stateMap}
-                </div>
+            </div>
         </div>
     )
 }
