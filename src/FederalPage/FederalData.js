48ea39975034ff1a950aecf4b7135e3ea550f1b0
Benji <bwolfenberger@gmail.com>
2021-11-04 16:58:33 -0700

    federal pages reverted back to match main

--- src/FederalPage/FederalData.js
+++ src/FederalPage/FederalData.js
@@ -1,38 +1,38 @@
-import React, { useRef, useState, useEffect } from 'react'
-import { IoIosArrowForward, IoIosArrowDown } from 'react-icons/io'
-import './FederalData.css'
-import { Link } from 'react-router-dom'
-import FederalRow from './FederalRow'
+import React, { useRef, useState, useEffect } from "react";
+import { IoIosArrowForward, IoIosArrowDown } from "react-icons/io";
+import "./FederalData.css";
+import { Link } from "react-router-dom";
+import FederalRow from "./FederalRow";
 
 function FederalData() {
-  const [federalData, setFederalData] = useState([])
-  const [expandNameContainer, setExpandNameContainer] = useState([])
-  const [show, setShow] = useState(true)
+  const [federalData, setFederalData] = useState([]);
+  const [expandNameContainer, setExpandNameContainer] = useState([]);
+  const [show, setShow] = useState(true);
 
   const getFederalData = async () => {
     await fetch(
-      'http://localhost:9000/federal',
+      "http://localhost:9000/federal",
 
       {
         headers: {
-          'Content-Type': 'application/json',
-          Accept: 'application/json',
+          "Content-Type": "application/json",
+          Accept: "application/json",
         },
       }
     )
       .then(function (response) {
-        // console.log(response)
-        return response.json()
+        console.log(response);
+        return response.json();
       })
       .then(function (myJson) {
-        // console.log(myJson)
-        setFederalData(myJson)
-      })
-  }
+        console.log(myJson);
+        setFederalData(myJson);
+      });
+  };
 
   useEffect(() => {
-    getFederalData()
-  }, [])
+    getFederalData();
+  }, []);
 
   const clickRightArrow = (
     <IoIosArrowForward
@@ -41,7 +41,7 @@ function FederalData() {
       color="gray"
       onClick={() => setExpandNameContainer(!expandNameContainer)}
     />
-  )
+  );
 
   const clickDownArrow = (
     <IoIosArrowDown
@@ -50,44 +50,44 @@ function FederalData() {
       color="gray"
       onClick={() => setExpandNameContainer(!expandNameContainer)}
     />
-  )
+  );
 
-  const closeNameContainer = () => setExpandNameContainer(false)
+  const closeNameContainer = () => setExpandNameContainer(false);
+
+
 
   return (
     <div className="federal-container">
       <div id="federal-header-container">
         <h1 id="federal-header">ADVANCED FEDERAL DATA SEARCH</h1>
       </div>
-
       <div className="federal-data-container">
         <div className="federal-filter-container"></div>
 
         <div id="data-table-container">
           <div>
-            <h3 className="spending-name-prime-award">
-              SPENDING BY PRIME AWARD
-            </h3>
+            <h3 className="spending-name-prime-award">SPENDING BY PRIME AWARD</h3>
           </div>
           <div id="header">
             <h3 className="table-header">Select</h3>
             <h3 className="table-header">Name</h3>
           </div>
-          {
-            <div>
-              {federalData.map((federalContent, i) => {
-                return (
-                  <div key={i}>
-                    <FederalRow federalContent={federalContent} />
-                  </div>
-                )
-              })}
-            </div>
-          }
+          {<div>
+      {federalData.map((federalContent, i) => {
+         return (
+           <div key={i}> 
+         <FederalRow
+          federalContent={federalContent}
+          />
+          </div>
+         )
+
+      })}
+      </div>}
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default FederalData
+export default FederalData;
--- src/FederalPage/FederalRow.js
+++ src/FederalPage/FederalRow.js
@@ -1,32 +1,13 @@
 import React, { useRef, useState } from "react";
+import StateData from "../StatePage/StateData";
 import { IoIosArrowForward, IoIosArrowDown } from "react-icons/io";
 
 const FederalRow = ({ federalContent }) => {
-  // const [isAnimating, setIsAnimating] = useState(false);
-  // const collapseRef = useRef(null);
-  // const contentRef = useRef(null);
-  const [isActive, setIsActive] = useState(false);
-  const [expandNameContainer, setExpandNameContainer] = useState([])
- 
-
-  const handleClick = () => {};
-
-  const handleTransition = () => {};
-
-
-  const clickRightArrow = <IoIosArrowForward
-  className="right-arrow" size="30px" color="grey"
-  onClick={() => setExpandNameContainer(!expandNameContainer)}
-/>
-
+	@@ -23,31 +22,31 @@
 const clickDownArrow = <IoIosArrowDown
   className="down-arrown" size="30px" color="grey"
   onClick={() => setExpandNameContainer(!expandNameContainer)}
 />
-
-
-
-
   return (
     // {console.log(content, 'getting the content')}
     
@@ -42,11 +23,11 @@ const clickDownArrow = <IoIosArrowDown
                     {
                         < ul>
                             <li className="recipientNumber">Recipent Name: {federalContent.Recepient_Name}</li>
-                            <li className="cfda">City: {federalContent.City}</li>
+                            <li className="cfda">City: {federalContent.Richmond}</li>
                             <li className="awardId">Country: {federalContent.Country} </li>
                             <li className="starliate">Funding Amount: {federalContent.Total_Award_Amount}</li>
                             <li className="county">Award Description: {federalContent.award_description}</li>
-                            <li className="awardAmount">Award Type: {federalContent.Award_Type}</li>
+                            <li className="awardAmount">Award Type: {federalContent.award_Type}</li>
                             <li className="awardType">Start date: {federalContent.Start_Date}</li>
                             <li className="awardType">End date: {federalContent.End_Date}</li>
                         </ul>
