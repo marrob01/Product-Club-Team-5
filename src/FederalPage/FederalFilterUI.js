c43d7896f9927faf7beeaa4eb5fcb48f9101a6ca
Sebastian G <gayle054@gmail.com>
2021-11-09 17:53:09 -0800

    fixed pagination button rerender

--- src/FederalPage/FederalFilterUI.js
+++ src/FederalPage/FederalFilterUI.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { IoIosArrowForward, IoIosArrowDown } from 'react-icons/io';
 
 function FederalFilterUI({ filter }) {
@@ -42,6 +42,45 @@ function FederalFilterUI({ filter }) {
     }
 
     console.log(value);
+
+
+
+    const Filter = ({newFilter}) => {
+
+        const [filteredDataList, setFilteredDataList] = useState(null)
+        // const [filter, setFilter] = useState(null)
+    
+    
+        // Award_Id: String,
+        //     Start_Date: Date,
+        //     End_Date: Date,
+        //     Recepient_Name: String,
+        //     Country: String,
+        //     City: String,
+        //     Federal_Covid_19_Obligation: Number,
+        //     Total_Award_Amount: Number,
+        //     cfda_title: String,
+        //     award_description: String,
+        //     Award_Type: String
+    
+        const handleFilterSearch = () => {
+            let filteredData = newFilter;
+            if (!newFilter) setFilteredDataList()
+            else {
+                if (newFilter.Start_Date) filteredData = filteredData.filter(award => award.Start_Date === newFilter.Start_Date)
+                if (newFilter.End_Date) filteredData = filteredData.filter(award => award.End_Date === newFilter.End_Date)
+                if (newFilter.Recepient_Name) filteredData = filteredData.filter(award => award.Recepient_Name === newFilter.Recepient_Name)
+                if (newFilter.City) filteredData = filteredData.filter(award => award.City === newFilter.City)
+                if (newFilter.Total_Award_Amount) filteredData = filteredData.filter(award => award.recipient_name === newFilter.recipient_name)
+                if (newFilter.Award_description) filteredData = filteredData.filter(award => award.Award_description === newFilter.Award_description)
+                if (newFilter.cfda_title) filteredData = filteredData.filter(award => award.cfda_title === newFilter.cfda_title)
+                setFilteredDataList(filteredData) 
+            }
+        }
+        
+        
+        if (filteredDataList) console.log(filteredDataList)
+    }
 
 
     return (
